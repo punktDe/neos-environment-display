b9b6fae61436cfdcce7838716b14db24681b860a
Sebastian Helzle <sebastian@helzle.it>
2025-01-16 14:49:44 +0100

    FEATURE: Secondary smaller label

--- Resources/Private/NeosEnvironmentDisplay/src/Components/EnvironmentDisplay.js
+++ Resources/Private/NeosEnvironmentDisplay/src/Components/EnvironmentDisplay.js
@@ -1,23 +1,28 @@
 import React, { PureComponent } from 'react';
-import styles from './EnvironmentDisplay.css';
 import PropTypes from 'prop-types';
-import {neos} from '@neos-project/neos-ui-decorators';
-import {Icon} from "@neos-project/react-ui-components";
+import { neos } from '@neos-project/neos-ui-decorators';
+import { Icon } from '@neos-project/react-ui-components';
+
+import './EnvironmentDisplay.css';
 
 @neos((globalRegistry) => ({
     displayConfiguration: globalRegistry.get('frontendConfiguration').get('PunktDe.NeosEnvironmentDisplay')
 }))
 export class EnvironmentDisplay extends PureComponent {
     static propTypes = {
-        displayConfiguration: PropTypes.object,
+        displayConfiguration: PropTypes.object
     };
 
     render() {
-        const { backgroundColor, label, icon } = this.props.displayConfiguration
+        const { backgroundColor, label, secondaryLabel, icon } = this.props.displayConfiguration;
 
         return (label !== ''
-            && <div className={styles.wrapper} style={{backgroundColor: backgroundColor}}>
-                {icon ? <Icon icon={icon} padded={'right'} /> : ''}<span className={styles.content}>{label}</span>
+            && <div className="environment-display-wrapper" style={{ backgroundColor: backgroundColor }}>
+                {icon ? <Icon icon={icon} padded={'right'} /> : ''}
+                <span className="environment-display-wrapper__content">
+                  {label}
+                  {secondaryLabel && (<small>{secondaryLabel}</small>)}
+                </span>
             </div>
         );
     }
